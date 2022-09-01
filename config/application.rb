8ebfe0f07f229beb8526b34a9ef0c7cb0d5aacf6
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-09-01 10:26:05 -0300

    initial config and index

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module RailsMarket
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.default_locale = :"pt-BR"
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
