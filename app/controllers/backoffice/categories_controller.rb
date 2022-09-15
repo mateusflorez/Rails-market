86b2cb57897bbb3ec5441a1c2c161cbb7d90984e
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-09-15 14:07:26 -0300

    new category + flash messages

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -5,14 +5,28 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = 'Category successfuly created.'
+      redirect_to backoffice_categories_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:description)
   end
 end
