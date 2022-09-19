4d105df960898f45ac79fc5d46256cac7a4ace4f
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-09-19 15:16:41 -0300

    admin management page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
 
+  namespace :backoffice do
+    get 'admins/index'
+  end
   get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
     resources :categories, except: [:show, :destroy]
     get 'dashboard', to: 'dashboard#index'
+    get 'admins/index'
   end
 
   namespace :site do
     get 'home', to: 'home#index'
   end
 
-  devise_for :admins
+  devise_for :admins, :skip => [:registration]
   devise_for :members
 
   root 'site/home#index'
