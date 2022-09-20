7fb62220d1ac9b440fd11857ef3a99f456cf6255
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-09-20 15:01:32 -0300

    scope

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,9 @@
 class Admin < ApplicationRecord
   enum role: [:administrator, :moderator]
+
+  #scope :moderators, -> { where(:role => 'administrator')}
+
+  # Ex:- scope :active, -> {where(:active => true)}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
