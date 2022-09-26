3fd90648d795b25b37cd858090180f1bb200b238
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-09-26 13:52:54 -0300

    role radio buttons

--- app/helpers/backoffice/admins_helper.rb
+++ app/helpers/backoffice/admins_helper.rb
@@ -1,2 +1,13 @@
 module Backoffice::AdminsHelper
+  OptionsForRoles = Struct.new(:id, :description)
+
+  def options_for_roles
+    options = []
+
+    Admin.roles.each do |key, value|
+      options.push(OptionsForRoles.new(key, value))
+    end
+
+    options
+  end
 end
