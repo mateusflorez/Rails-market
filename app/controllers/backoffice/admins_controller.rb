07e17c5fed557c96e48bd6380626dd9f5bfa30bf
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-10-19 14:02:42 -0300

    pundit error fix

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -54,7 +54,11 @@ class Backoffice::AdminsController < BackofficeController
   end
 
   def admin_params
-    params.require(:admin).permit(policy(@admin).permitted_attributes)
+    if @admin.blank?
+      params.require(:admin).permit(:name, :email, :role, :password, :password_confirmation)
+    else
+      params.require(:admin).permit(policy(@admin).permitted_attributes)
+    end
   end
 
   def password_blank?
